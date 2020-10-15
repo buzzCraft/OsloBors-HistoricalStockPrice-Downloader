eb74ad50684c2767db4b379e821e18ca913129d1
buzzCraft <67235613+buzzCraft@users.noreply.github.com>
2020-10-15 19:29:28 +0200

    Minor bug fix

--- main.py
+++ main.py
@@ -2,22 +2,14 @@
 """
 Created on Wed Oct 14 16:03:19 2020
 
-
-First run:
-    You need to have a folder called /data/comb/
-    in the same folder you have the python files in
-
 Usage:
-    downloader.stockDataDownloader() take save file path as argument, and download
-    xlsx files from OsloBors, it then convert these files to csv and delete the xlsx files
+    Import Ticks from ./Tick.csv
+    Alter the file for fewer stocks
     
-    merger.merge() take the path of the saved csv's and merge them into one big df, if set to
-    True, it also save a csv version of the df to /data/comb/. I find this usefull when doing
-    quick checkups on the data.
-    
-Plans:
-    Im planning to make a downloader class and a much more generic datawrangler class, and at the
-    same time create a stock class.
+    To create stock obj, call stockClass.Stock(TICKER)
+    To plot stockdata, call stock.plot()
+    To merge all data to one dataframe, send a list of 
+    stock obj to merger.merge(LIST)
     
 
 """
@@ -42,18 +34,20 @@ with open('./Tick.csv', 'r') as f:
     liste = f.readlines()
     for item in liste:
         tickerList.append(item.rstrip("\n")) #Stripping newline in csv
+        
 print(tickerList)
 stockList = []
+
 for TICKER in tickerList:
     stockList.append(stockClass.Stock(TICKER))
 date = True
-for stock in stockList:
+# for stock in stockList:
     
-    stock.downloadData(date)
-    stock.readData()
-    print(stock.data.head())
-    stock.plotData()                                     #Time consuming!!
-    date = False
+    # stock.downloadData(date)
+    # stock.readData()
+    # print(stock.data.head())
+    # stock.plotData()                                     #Time consuming!!
+    # date = False
     
 merger.merge(stockList, True)
 # data.downloadData()
--- stockClass.py
+++ stockClass.py
@@ -20,7 +20,8 @@ class Stock():
         self.TICKER = TICKER                                                   #Set the ticker for instance
         self.url = f'https://www.oslobors.no/ob/servlets/excel?type=history&columns=DATE%2C+CLOSE_CA%2C+BID_CA%2C+ASK_CA%2C+HIGH_CA%2C+LOW_CA%2C+TURNOVER_TOTAL%2C+VOLUME_TOTAL_CA%2C+TRADES_COUNT%2C+TRADES_COUNT_TOTAL%2C+VWAP_CA&format[DATE]=ddd.mm.YY&format[CLOSE_CA]=%23%2C%23%230.00%23%23%23&format[BID_CA]=%23%2C%23%230.00%23%23&format[ASK_CA]=%23%2C%23%230.00%23%23&format[HIGH_CA]=%23%2C%23%230.00%23%23%23&format[LOW_CA]=%23%2C%23%230.00%23%23%23&format[TURNOVER_TOTAL]=%23%2C%23%230&format[VOLUME_TOTAL_CA]=%23%2C%23%230&format[TRADES_COUNT]=%23%2C%23%230&format[TRADES_COUNT_TOTAL]=%23%2C%23%230&format[VWAP_CA]=%23%2C%23%230.00%23%23%23&header[DATE]={self.TICKER}&header[CLOSE_CA]=Siste&header[BID_CA]=Kj%C3%B8per&header[ASK_CA]=Selger&header[HIGH_CA]=H%C3%B8y&header[LOW_CA]=Lav&header[TURNOVER_TOTAL]=Totalt%20omsatt%20%28NOK%29&header[VOLUME_TOTAL_CA]=Totalt%20antall%20aksjer%20omsatt&header[TRADES_COUNT]=Antall%20off.%20handler&header[TRADES_COUNT_TOTAL]=Antall%20handler%20totalt&header[VWAP_CA]=VWAP&view=DELAYED&source=feed.ose.quotes.INSTRUMENTS&filter=ITEM_SECTOR%3D%3Ds{self.TICKER}.OSE%26%26DELETED!%3Dn1&stop=now&start=1441317600000&space=DAY&ascending=true&filename=data.xlsx'
         self.path = os.path.join(os.getcwd(), 'data')                          #Path, could be global?
-        self.filepath =  os.path.join(self.path, f'{TICKER}.csv')                        
+        self.filepath =  os.path.join(self.path, f'{TICKER}.csv') 
+        self.date = False                    
         if os.path.exists(self.filepath):                                      #Check if we have a csv file with tick name
             print("Data excists, using that file")
             self.readData()                                                    #Load that file
